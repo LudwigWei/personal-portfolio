baaa0a6b4487619999ee2c1aa8b4ba21a8296331
LudwigWei <louis.vncnt.crisaldo@gmail.com>
2026-05-26 18:34:10 +0800

    fix: corrected the config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { nitro } from "nitro/vite"; 
+import tailwindcss from "@tailwindcss/vite"; 
 
 export default defineConfig({
   plugins: [
+    tailwindcss(), // 1. MOVE THIS TO THE VERY TOP
     tsconfigPaths(),
     tanstackStart({
       server: {
